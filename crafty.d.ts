611477bc9eb70658b95f17d94bb3be092c1cb93e
Silent K <silentksionda@gmail.com>
2015-11-21 13:09:47 -0700

    Updating definitions.

--- crafty.d.ts
+++ crafty.d.ts
@@ -74,15 +74,43 @@ declare module CraftyJS {
         attr(map: Map, silent?: boolean, recursive?: boolean): T;
 
         // // Event binding
-        bind(eventName: 'MouseOver', callback: (event: MouseEvent) => void);
-        bind(eventName: 'MouseOut', callback: (event: MouseEvent) => void);
-        bind(eventName: 'MouseDown', callback: (event: MouseEvent) => void);
-        bind(eventName: 'MouseUp', callback: (event: MouseEvent) => void);
-        bind(eventName: 'Click', callback: (event: MouseEvent) => void);
-        bind(eventName: 'DoubleClick', callback: (event: MouseEvent) => void);
-        bind(eventName: 'MouseMove', callback: (event: MouseEvent) => void);
+        // // // Mouse
+        bind(eventName: 'MouseOver', callback: (event: MouseEvent) => void): T;
+        bind(eventName: 'MouseOut', callback: (event: MouseEvent) => void): T;
+        bind(eventName: 'MouseDown', callback: (event: MouseEvent) => void): T;
+        bind(eventName: 'MouseUp', callback: (event: MouseEvent) => void): T;
+        bind(eventName: 'Click', callback: (event: MouseEvent) => void): T;
+        bind(eventName: 'DoubleClick', callback: (event: MouseEvent) => void): T;
+        bind(eventName: 'MouseMove', callback: (event: MouseEvent) => void): T;
 
-        bind(eventName: string, callback: (...data: any[]) => void);
+        // // // Keyboard
+        bind(eventName: 'KeyDown', callback: (event: KeyboardEvent) => void): T;
+        bind(eventName: 'KeyUp', callback: (event: KeyboardEvent) => void): T;
+
+        // // // Collision
+        bind(eventName: 'HitOn', callback: (data: CollisionObject[]) => void): T;
+        bind(eventName: 'HitOff', callback: () => void): T;
+
+        // // // Generic
+        bind(eventName: string, callback: (...data: any[]) => void): T;
+
+        // Destroy
+        destroy(): void;
+    }
+
+    interface TwoDComponent<T> extends Core<T> {
+        x: number;
+        y: number;
+        h: number;
+        w: number;
+        z: number;
+        rotation: number;
+        alpha: number;
+        visible: boolean;
+    }
+
+    interface KeyboardComponent<T> extends Core<T> {
+        isDown(key: string): boolean;
     }
 
     interface TextComponent<T> extends Core<T> {
@@ -99,11 +127,7 @@ declare module CraftyJS {
         color(): string;
     }
 
-    interface FourwayComponent<T> extends MotionComponent<T>, Core<T> {
-        fourway(speed: number): T;
-    }
-
-    interface MotionComponent<T> extends Core<T> {
+    interface MotionComponent<T> extends TwoDComponent<T>, Core<T> {
         resetMotion(): T;
         motionDelta(): {x: number, y: number};
         velocity(): {x: number, y: number};
@@ -114,11 +138,38 @@ declare module CraftyJS {
         ay: number;
     }
 
+    interface FourwayComponent<T> extends MotionComponent<T>, Core<T> {
+        fourway(speed: number): T;
+    }
+
+    interface CollisionObject {
+        obj: AnyEntity;
+        type: string;
+        overlap: number;
+    }
+    interface CollisionCallback {
+        (collisionObject: CollisionObject): void
+    }
+    interface CollisionComponent<T> extends TwoDComponent<T>, Core<T> {
+        // TODO: CraftyJS.polygon definition
+        collision(coordinatePairs: number[]): T;
+
+        // TODO: CraftyJS.collision object interface
+        hit(component: string): boolean|CollisionObject[];
+
+        // TODO: Verify callOff function definition
+        onHit(comp: string, callOn: CollisionCallback[], callOff: () => void): T;
+
+        checkHits(componentList: string): T;
+        checkHits(...componentList: string[]): T;
+    }
+
     /*
     Entities could have any number of defined functions depending on their
     components
     */
     interface AnyEntity extends
+        TwoDComponent<AnyEntity>,
         TextComponent<AnyEntity>,
         ColorComponent<AnyEntity>,
         FourwayComponent<AnyEntity>,
