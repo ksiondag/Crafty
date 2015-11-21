db54e77ad1c7016edc810aab5efc3e7727791aa1
Silent K <silentksionda@gmail.com>
2015-11-20 18:36:09 -0700

    Updating definitions file

--- crafty.d.ts
+++ crafty.d.ts
@@ -5,21 +5,46 @@
 
 declare module CraftyJS {
 
+    // General types
+    type NotFunction = number|string|boolean|Array<any>|NotFunctionObject;
+
+    interface NotFunctionObject {
+        [index: string]: NotFunction;
+    }
+
+    interface Map {
+        [index: string]: any;
+    }
+
+    // Crafty-specific types
+    interface ComponentMap extends Map {
+        // Stuff Crafty itself checks for
+        init?(): void;
+        required?: string;
+    }
+
+    interface TextMap {
+        type?: string;
+        weight?: string;
+        size?: string;
+        family?: string;
+        lineHeight?: string;
+        variant?: string;
+    }
+
+    // Function types
     interface DefineScene {
         (name: string, init: (data?: any) => void, uninit?: () => void): void;
     }
 
-    type NotFunction = number|string|boolean|Array<any>;
-    interface NotFunctionObject {
-        [index: string]: NotFunction;
-    }
     interface EnterScene {
-        (name: string, data?: NotFunction|NotFunctionObject): void;
+        (name: string, data?: NotFunction): void;
     }
 
     interface DefineOrEnterScene extends DefineScene, EnterScene {
     }
 
+    // Crafty's functions
     interface Static {
         (selector: string): AnyEntity;
         (id: number): AnyEntity;
@@ -27,7 +52,7 @@ declare module CraftyJS {
         init(width: number, height: number, divId: string): void;
 
         e(...components: string[]): AnyEntity;
-        c(name: string, definition: Component): void;
+        c(name: string, definition: ComponentMap): void;
 
         // Repeated definitions here because Function Types can't be overloaded
         scene: DefineOrEnterScene;
@@ -36,45 +61,59 @@ declare module CraftyJS {
 
     }
 
-    interface Component {
-        // Stuff Crafty itself checks for
-        init?(): void;
-        required?: string;
-
-        // Components are user-defined objects that plug into entities
-        // Very flexible creation
-        [index: string]: any;
-    }
-
-    /*
-    All entities have these properties
-    */
+    // All entities have these properties
     interface Core<T> {
         addComponent(...components: string[]): void;
         requires(components: string): void;
 
-        // Setting values
-        attr(property: string, value: any, silent?: boolean, recursive?: boolean): T;
-        attr(map: Object, silent?: boolean, recursive?: boolean): T;
-
         // Getting values
         attr(property: string): any;
+
+        // Chaining Functions
+        //  // Setting values
+        attr(prop: string, val: any, silent?: boolean, recursive?: boolean): T;
+        attr(map: Map, silent?: boolean, recursive?: boolean): T;
+
+        // // Event binding
+        bind(eventName: 'MouseOver', callback: (event: MouseEvent) => void);
+        bind(eventName: 'MouseOut', callback: (event: MouseEvent) => void);
+        bind(eventName: 'MouseDown', callback: (event: MouseEvent) => void);
+        bind(eventName: 'MouseUp', callback: (event: MouseEvent) => void);
+        bind(eventName: 'Click', callback: (event: MouseEvent) => void);
+        bind(eventName: 'DoubleClick', callback: (event: MouseEvent) => void);
+        bind(eventName: 'MouseMove', callback: (event: MouseEvent) => void);
+
+        bind(eventName: string, callback: (...data: any[]) => void);
     }
 
     interface TextComponent<T> extends Core<T> {
         text(value: string): T;
-        textFont(value: TextFont): T;
+        textColor(color: string): T;
+        textFont(key: string, value: string): T;
+        textFont(value: TextMap): T;
+        unselectable(): T;
+    }
+
+    interface ColorComponent<T> extends Core<T> {
+        color(value: string, strength?: number): T;
+        color(red: number, green: number, blue: number, strength?: number): T;
+        color(): string;
+    }
+
+    interface FourwayComponent<T> extends Core<T> {
+        fourway(speed: number): T;
     }
 
     /*
     Entities could have any number of defined functions depending on their
     components
     */
-    interface AnyEntity extends TextComponent<AnyEntity>, Core<AnyEntity> {
-    }
-
-    interface TextFont {
-        size?: string
+    interface AnyEntity extends
+        TextComponent<AnyEntity>,
+        ColorComponent<AnyEntity>,
+        FourwayComponent<AnyEntity>,
+        Core<AnyEntity>
+    {
     }
 }
 
